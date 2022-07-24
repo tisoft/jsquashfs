bb05a5c163cf4a0d26f15c90a92a8c62f4d1a597
Markus Heberling <markus@heberling.net>
2022-07-24 20:30:58 +0200

    shrink uncompressed lz4 result

--- core/src/main/java/de/tisoft/jsquashfs/compression/lz4/Lz4.java
+++ core/src/main/java/de/tisoft/jsquashfs/compression/lz4/Lz4.java
@@ -11,9 +11,20 @@ import java.util.zip.InflaterInputStream;
 public class Lz4 implements Decompressor {
     @Override
     public byte[] uncompress(byte[] rawData, int maxSize, boolean padded) {
-            final byte[] dest = new byte[maxSize];
-            LZ4Factory.safeInstance().safeDecompressor().decompress(rawData, dest);
-
+        final byte[] dest = new byte[maxSize];
+        int len = LZ4Factory.fastestInstance().safeDecompressor().decompress(rawData, dest);
+        if (len == maxSize) {
+            // maxSize reached, the buffer can be returned as is
             return dest;
+        } else if (padded) {
+            // pad the remaining contents of the buffer with 0
+            Arrays.fill(dest, len, dest.length - 1, (byte) 0);
+            return dest;
+        } else {
+            // shrink to actual size
+            byte[] bytes = new byte[len];
+            System.arraycopy(dest, 0, bytes, 0, bytes.length);
+            return bytes;
+        }
     }
 }
