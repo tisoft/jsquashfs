2fdcd57be3876cf14038030b8ef64973e1174b43
Markus Heberling <markus@heberling.net>
2022-07-25 08:04:10 +0200

    extract files

--- unsquashfs/src/main/java/de/tisoft/jsquashfs/Unsquashfs.java
+++ unsquashfs/src/main/java/de/tisoft/jsquashfs/Unsquashfs.java
@@ -1,7 +1,9 @@
 package de.tisoft.jsquashfs;
 
+import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Field;
+import java.nio.file.Files;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -9,14 +11,17 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import de.tisoft.jsquashfs.parser.Squashfs;
+import org.slieb.throwables.ConsumerWithThrowable;
 import picocli.CommandLine;
+
+import static org.slieb.throwables.ConsumerWithThrowable.aConsumerThatUnsafelyThrowsUnchecked;
 
 // This is a console application, so using System.out is fine
 @SuppressWarnings("java:S106")
 @CommandLine.Command(name = "unsquashfs", sortOptions = false)
 public class Unsquashfs implements Runnable{
 
-    @CommandLine.Option(names = {"${picocli.version.name.0:--v}", "${picocli.version.name.1:---version}"}, versionHelp = true, descriptionKey = "mixinStandardHelpOptions.version",
+    @CommandLine.Option(names = {"${picocli.version.name.0:--v}", "${picocli.version.name.1:--version}"}, versionHelp = true, descriptionKey = "mixinStandardHelpOptions.version",
             description = "Print version information and exit.")
     private boolean versionRequested;
 
@@ -28,8 +33,8 @@ public class Unsquashfs implements Runnable{
     @CommandLine.Parameters(paramLabel = "[files to extract or exclude (with -excludes) or cat (with -cat )]", hideParamSyntax = true)
     private String[] files;
 
-    @CommandLine.Option(names = { "-s", "--font-size" }, description = "Font size")
-    int fontSize = 19;
+    @CommandLine.Option(names = {"-d", "-dest"}, paramLabel = "pathname", arity = "1", description = "extract to <pathname>, default \"squashfs-root\"", defaultValue = "squashfs-root")
+    private String dest;
 
 
     @CommandLine.Option(names = {"${picocli.help.name.0:--h}", "${picocli.help.name.1:---help}"}, usageHelp = true, descriptionKey = "mixinStandardHelpOptions.help",
@@ -53,40 +58,48 @@ public class Unsquashfs implements Runnable{
 
             Squashfs.InodeHeader rootInode = squashfs.superblock().rootInodeRef().inodeTable().inodeHeader();
 
-            recurse(inodes, rootInode, "");
+            recurse(inodes, rootInode, new File(dest));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
     }
 
-    public static void recurse(Map<Long, Squashfs.InodeHeader> inodes, Squashfs.InodeHeader inodeHeader, String prefix) {
-        System.out.println(prefix + inodeHeader.inodeNumber() + " " + inodeHeader.type());
+    public static void recurse(Map<Long, Squashfs.InodeHeader> inodes, Squashfs.InodeHeader inodeHeader, File dest) throws IOException {
+        System.out.println(dest + " " + inodeHeader.inodeNumber() + " " + inodeHeader.type());
         if (inodeHeader.type() == Squashfs.InodeType.BASIC_DIRECTORY) {
+            if (!dest.isDirectory() && !dest.mkdir()) {
+                throw new IOException("Could not create directory " + dest);
+            }
             Squashfs.InodeHeaderBasicDirectory directory = (Squashfs.InodeHeaderBasicDirectory) inodeHeader.header();
             directory.dir().directoryHeader().stream()
                     .map(Squashfs.DirectoryHeader::directoryEntry)
                     .filter(Objects::nonNull)
                     .flatMap(List::stream)
-                    .forEach(directoryEntry -> {
-                        System.out.println(prefix + directoryEntry.name());
-                        recurse(inodes, inodes.get((long) directoryEntry._parent().inodeNumber() + directoryEntry.inodeOffset()), prefix + " ");
-                    });
+                    .forEach(aConsumerThatUnsafelyThrowsUnchecked(directoryEntry -> {
+                        File dir = new File(dest, directoryEntry.name());
+                        System.out.println(dir);
+                        recurse(inodes, inodes.get((long) directoryEntry._parent().inodeNumber() + directoryEntry.inodeOffset()), dir);
+                    }));
             clearMemory(directory, "dir", "_raw_dir", "directoryTable", "_raw_directoryTable");
         } else if (inodeHeader.type() == Squashfs.InodeType.EXTENDED_DIRECTORY) {
+            if (!dest.isDirectory() && !dest.mkdir()) {
+                throw new IOException("Could not create directory " + dest);
+            }
             Squashfs.InodeHeaderExtendedDirectory directory = (Squashfs.InodeHeaderExtendedDirectory) inodeHeader.header();
             directory.dir().directoryHeader().stream()
                     .map(Squashfs.DirectoryHeader::directoryEntry)
                     .filter(Objects::nonNull)
                     .flatMap(List::stream)
-                    .forEach(directoryEntry -> {
-                        System.out.println(prefix + directoryEntry.name());
-                        recurse(inodes, inodes.get((long) directoryEntry._parent().inodeNumber() + directoryEntry.inodeOffset()), prefix + " ");
-                    });
+                    .forEach(aConsumerThatUnsafelyThrowsUnchecked(directoryEntry -> {
+                        File dir = new File(dest, directoryEntry.name());
+                        System.out.println(dir);
+                        recurse(inodes, inodes.get((long) directoryEntry._parent().inodeNumber() + directoryEntry.inodeOffset()), dir);
+                    }));
             clearMemory(directory, "dir", "_raw_dir", "directoryTable", "_raw_directoryTable");
         } else if (inodeHeader.type() == Squashfs.InodeType.BASIC_FILE) {
             Squashfs.InodeHeaderBasicFile file = (Squashfs.InodeHeaderBasicFile) inodeHeader.header();
-            System.out.println(prefix + "size: " + file.fileSize());
+            System.out.println(dest + " size: " + file.fileSize());
             byte[] data = new byte[(int) file.fileSize()];
             int offset = 0;
             for (Squashfs.DataBlock block : file.blocks()) {
@@ -98,13 +111,14 @@ public class Unsquashfs implements Runnable{
             if (file.fragIndex() != 0xFFFFFFFFL) {
                 System.arraycopy(file.fragment().block().data().data(), (int) file.blockOffset(), data, offset, data
                         .length - offset);
-                System.out.println(prefix + "fragment: " + offset);
+                System.out.println(dest + " fragment: " + offset);
             } else {
-                System.out.println(prefix + "finished: " + offset);
+                System.out.println(dest + " finished: " + offset);
             }
+            Files.write(dest.toPath(), data);
             clearMemory(file, "blocks", "_raw_blocks", "fragment");
         } else {
-            throw new IllegalArgumentException("Unsupported inode type " + inodeHeader.type());
+            throw new IOException("Unsupported inode type " + inodeHeader.type());
         }
     }
 
--- unsquashfs/src/test/java/de/tisoft/jsquashfs/UnsquashfsTest.java
+++ unsquashfs/src/test/java/de/tisoft/jsquashfs/UnsquashfsTest.java
@@ -1,19 +1,24 @@
 package de.tisoft.jsquashfs;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-
-import com.ginsberg.junit.exit.ExpectSystemExit;
-import com.ginsberg.junit.exit.ExpectSystemExitWithStatus;
-import de.tisoft.jsquashfs.parser.Squashfs;
-import io.kaitai.struct.RandomAccessFileKaitaiStream;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
-@ExpectSystemExitWithStatus(0)
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+import static com.github.stefanbirkner.systemlambda.SystemLambda.catchSystemExit;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+
 class UnsquashfsTest {
+    @TempDir
+    private Path directory;
 
     public File targetDir() {
         String relPath = getClass().getProtectionDomain().getCodeSource().getLocation().getFile();
@@ -22,39 +27,77 @@ class UnsquashfsTest {
     }
 
     @Test
-    void testGzip() throws IOException {
+    void testGzip() throws Exception {
         test("sq.img.gzip");
     }
 
     @Test
-    void testLz4() throws IOException {
+    void testLz4() throws Exception {
         test("sq.img.lz4");
     }
 
     @Test
-    void testLzma() throws IOException {
+    void testLzma() throws Exception {
         test("sq.img.lzma");
     }
 
     @Test
-    void testLzo() throws IOException {
+    void testLzo() throws Exception {
         test("sq.img.lzo");
     }
 
     @Test
-    void testXz() throws IOException {
+    void testXz() throws Exception {
         test("sq.img.xz");
     }
 
     @Test
-    void testZstd() throws IOException {
+    void testZstd() throws Exception {
         test("sq.img.zstd");
     }
 
-    private void test(String name) throws IOException {
-        Unsquashfs.main(new String[]{new File(targetDir(), name).getAbsolutePath()});
+    private void test(String name) throws Exception {
+        int statusCode = catchSystemExit(() -> Unsquashfs.main(new String[]{new File(targetDir(), name).getAbsolutePath(), "-d", directory.toString()}));
+        assertThat(statusCode).isZero();
+        assertDirectory(new File(targetDir(), "data").toPath(), directory);
+    }
 
+    private void assertDirectory(Path source, Path dest) {
+        System.out.println("Checking " + dest);
+        File sourceFile = source.toFile();
+        File destFile = dest.toFile();
+        if (Files.isDirectory(source)) {
+            assertThat(dest).isDirectory();
+            assertThat(destFile.list()).containsExactlyInAnyOrder(sourceFile.list());
+            for (String s : sourceFile.list()) {
+                assertDirectory(new File(sourceFile, s).toPath(), new File(destFile, s).toPath());
+            }
+        } else if (Files.isRegularFile(source)) {
+            assertThat(dest).isRegularFile();
+            if (!Arrays.equals(digest(source), digest(dest))) {
+                assertThat(dest).hasSameBinaryContentAs(source);
+            }
+        } else {
+            fail("Unknown file type for " + source);
+        }
+    }
 
-   //     Assertions.assertThat(new File()).ishasSameContentAs();
+    private static byte[] digest(Path path) {
+        try (FileInputStream fis = new FileInputStream(path.toFile())) {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+
+            byte[] dataBytes = new byte[1024];
+
+            int nread;
+            while ((nread = fis.read(dataBytes)) != -1) {
+                md.update(dataBytes, 0, nread);
+            }
+
+            return md.digest();
+        } catch (IOException | NoSuchAlgorithmException e) {
+            fail("Can't calculate digest for " + path, e);
+            // never reached
+            throw new RuntimeException(e);
+        }
     }
 }
