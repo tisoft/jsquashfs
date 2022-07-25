a725be9610f64b16e661bee662dab304100c8da3
Markus Heberling <markus@heberling.net>
2022-07-25 12:32:49 +0200

    implement -version option

--- unsquashfs/src/main/java/de/tisoft/jsquashfs/Unsquashfs.java
+++ unsquashfs/src/main/java/de/tisoft/jsquashfs/Unsquashfs.java
@@ -2,6 +2,7 @@ package de.tisoft.jsquashfs;
 
 import static org.slieb.throwables.ConsumerWithThrowable.aConsumerThatUnsafelyThrowsUnchecked;
 
+import de.tisoft.jsquashfs.compression.lzo.LzoAvailabilityCheck;
 import de.tisoft.jsquashfs.parser.Squashfs;
 import java.io.File;
 import java.io.IOException;
@@ -11,13 +12,74 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.function.Function;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
 import java.util.stream.Collectors;
 import picocli.CommandLine;
 
 // This is a console application, so using System.out is fine
 @SuppressWarnings("java:S106")
-@CommandLine.Command(name = "unsquashfs", sortOptions = false)
+@CommandLine.Command(
+    name = "unsquashfs",
+    sortOptions = false,
+    versionProvider = Unsquashfs.Version.class)
 public class Unsquashfs implements Runnable {
+
+  static class Version implements CommandLine.IVersionProvider {
+
+    @Override
+    public String[] getVersion() throws Exception {
+      Manifest manifest = new Manifest(getClass().getResourceAsStream("/META-INF/MANIFEST.MF"));
+      Attributes attr = manifest.getMainAttributes();
+      if (LzoAvailabilityCheck.isLzoAvailable()) {
+        return new String[] {
+          "junsquashfs version "
+              + attr.getValue("Implementation-Version")
+              + " ("
+              + attr.getValue("Implementation-Build-Date")
+              + ") with LZO support",
+          "copyright (C) 2022 Markus Heberling <markus@tisoft.de>",
+          "",
+          "This program is free software; you can redistribute it and/or",
+          "modify it under the terms of the GNU General Public License",
+          "as published by the Free Software Foundation; either version 2,",
+          "or (at your option) any later version.",
+          "",
+          "This program is distributed in the hope that it will be useful,",
+          "but WITHOUT ANY WARRANTY; without even the implied warranty of",
+          "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the",
+          "GNU General Public License for more details."
+        };
+      } else {
+        return new String[] {
+          "junsquashfs version "
+              + attr.getValue("Implementation-Version")
+              + " ("
+              + attr.getValue("Implementation-Build-Date")
+              + ")",
+          "copyright (C) 2022 Markus Heberling <markus@tisoft.de>",
+          "",
+          "Permission is hereby granted, free of charge, to any person obtaining a copy",
+          "of this software and associated documentation files (the \"Software\"), to deal",
+          "in the Software without restriction, including without limitation the rights",
+          "to use, copy, modify, merge, publish, distribute, sublicense, and/or sell",
+          "copies of the Software, and to permit persons to whom the Software is",
+          "furnished to do so, subject to the following conditions:",
+          "",
+          "The above copyright notice and this permission notice shall be included in all",
+          "copies or substantial portions of the Software.",
+          "",
+          "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR",
+          "IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,",
+          "FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE",
+          "AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER",
+          "LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,",
+          "OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE",
+          "SOFTWARE."
+        };
+      }
+    }
+  }
 
   @CommandLine.Option(
       names = {"${picocli.version.name.0:--v}", "${picocli.version.name.1:--version}"},
